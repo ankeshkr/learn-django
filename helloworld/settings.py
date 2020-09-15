d97094c3db4cd718e02bf61e4228ce472214d474
ankeshkr <32651031+ankeshkr@users.noreply.github.com>
2020-09-16 00:48:14 +0530

    Update settings.py
    adding the allowed host and changing debug to false

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fwoyimnsz!^k(e&9s1nj_xdh6-26mntu!h3=o9x+yw_4i*wprc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['learn-django1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
